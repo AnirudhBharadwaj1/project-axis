120a8c24fa1a58fbd4039eca3c881ffd88837690
AnirudhBharadwaj1 <emailanirudhb@gmail.com>
2025-08-14 15:55:48 -0400

    most popular kits section almost done

--- src/components/BestSelling.jsx
+++ src/components/BestSelling.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import "../styles/BestSelling.css";
+
+import gladius from "../assets/temp/gladius.jpeg";
 
 function BestSelling({ products }) {
     // Get the top three products sold
@@ -6,12 +9,34 @@ function BestSelling({ products }) {
         return products.sort((a, b) => b.numSold - a.numSold).slice(0, 3);
     };
 
+    const [hovered, setHovered] = useState(null);
+
     return (
         <div className="best-selling-div">
             <h3 className="section-title">Most Popular Kits</h3>
-            {getBestSellers().map((product, key) => (
-                <p key={key}>{product.name}</p>
-            ))}
+            <div className="best-selling-display">
+                {getBestSellers().map((product, key) => (
+                    <a
+                        key={key}
+                        href={`/product/` + product.id}
+                        className={`best-selling-product ${
+                            hovered === key ? "hovered" : ""
+                        }`}
+                        style={{ "--glow": product.background }}
+                        onMouseEnter={() => setHovered(key)}
+                        onMouseLeave={() => setHovered(null)}
+                    >
+                        <img
+                            src={gladius}
+                            alt="Product Display"
+                            className="best-selling-image"
+                        />
+                        <h3 className="best-selling-header">
+                            {product.name.toUpperCase()}
+                        </h3>
+                    </a>
+                ))}
+            </div>
         </div>
     );
 }
