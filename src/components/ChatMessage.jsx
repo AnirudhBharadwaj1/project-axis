bc9552be06a66fe11ccebbeffc75362815c0ac9a
AnirudhBharadwaj1 <emailanirudhb@gmail.com>
2025-10-30 23:27:39 -0400

    Finished styling for the chatbot

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -2,7 +2,13 @@ import "../styles/Chat.css";
 
 function ChatMessage({ message }) {
     return (
-        <div className="chat-message">
+        <div
+            className="chat-message"
+            style={{
+                justifyContent:
+                    message.sender === "user" ? "flex-end" : "flex-start",
+            }}
+        >
             <p className="chat-text">{message.text}</p>
         </div>
     );
